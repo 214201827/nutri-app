2e8a2c3877802569e529f6ec55681db750d77a4e
David Munguía Acosta <a214201827@unison.mx>
2024-09-14 01:51:39 -0700

    Otro intento de Login.

--- app/src/main/java/com/example/nutriapp/MainActivity.kt
+++ app/src/main/java/com/example/nutriapp/MainActivity.kt
@@ -4,12 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.nutriapp.ui.theme.NutriappTheme
 
@@ -38,10 +46,40 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+@Composable
+fun OutLineTextFieldSample() {
+    Column {
+        var text by remember { mutableStateOf(TextFieldValue("")) }
+        OutlinedTextField(
+            value = text,
+            label = { Text(text = "Email") },
+            onValueChange = {
+                text = it
+            }
+        )
+        OutlinedTextField(
+            value = text,
+            label = { Text(text = "Password") },
+            onValueChange = {
+                text = it
+            }
+        )
+
+    }
+}
+
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     NutriappTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun FieldPreview() {
+    NutriappTheme {
+        OutLineTextFieldSample()
     }
 }
