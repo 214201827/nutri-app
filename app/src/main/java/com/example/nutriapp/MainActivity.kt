740ba0f3aabe8af9fd8e9b85023817b8c4b3d9a4
David Munguía Acosta <a214201827@unison.mx>
2024-09-15 12:33:08 -0700

    Otro intento de Login, esta vez no salio tan mal.

--- app/src/main/java/com/example/nutriapp/MainActivity.kt
+++ app/src/main/java/com/example/nutriapp/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
@@ -16,28 +21,37 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.nutriapp.ui.theme.NutriappTheme
+import org.w3c.dom.Text
 
+
+
+// Clase de actividad, aquí estará lo que se ejecutará.
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             NutriappTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
+
+                    /*Greeting(
                         name = "Android",
                         modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                    )*/
+                    LoginComponent()
+
+
             }
         }
     }
 }
 
+// Texto de prueba
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
@@ -46,33 +60,52 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
     )
 }
 
+// Boton iniciar sesión
 @Composable
-fun OutLineTextFieldSample() {
-    Column {
-        var text by remember { mutableStateOf(TextFieldValue("")) }
-        OutlinedTextField(
-            value = text,
-            label = { Text(text = "Email") },
-            onValueChange = {
-                text = it
-            }
-        )
-        OutlinedTextField(
-            value = text,
-            label = { Text(text = "Password") },
-            onValueChange = {
-                text = it
-            }
-        )
-
+fun BotonIniciarSesion(onClick: () -> Unit) {
+    Button(onClick = { onClick() },  modifier = Modifier.fillMaxWidth()) {
+        Text("Iniciar sesión")
     }
 }
 
-@Preview(showBackground = true)
+
+// Botón Iniciar con Google
 @Composable
-fun GreetingPreview() {
-    NutriappTheme {
-        Greeting("Android")
+fun BotonIniciarGooglen(onClick: () -> Unit) {
+    Button(onClick = { onClick() }, modifier = Modifier.fillMaxWidth()) {
+        Text("Iniciar con Google")
+    }
+}
+
+// Elemento de login, donde estan los fields y botones
+@Composable
+fun LoginComponent(modifier: Modifier = Modifier) {
+    Column (
+        modifier = modifier
+
+    ) {
+        var email by remember { mutableStateOf(TextFieldValue("")) }
+        var pass by remember { mutableStateOf(TextFieldValue("")) }
+        OutlinedTextField(
+            modifier = modifier,
+            value = email,
+            label = { Text(text = "Email") },
+            onValueChange = {
+                email = it
+            }
+        )
+        OutlinedTextField(
+            modifier = modifier,
+            value = pass,
+            label = { Text(text = "Contraseña") },
+            onValueChange = {
+                pass = it
+            }
+        )
+        BotonIniciarSesion {  }
+
+        BotonIniciarGooglen {  }
+
     }
 }
 
@@ -80,6 +113,22 @@ fun GreetingPreview() {
 @Composable
 fun FieldPreview() {
     NutriappTheme {
-        OutLineTextFieldSample()
+
+            /*Greeting(
+                name = "Android",
+                modifier = Modifier.padding(innerPadding)
+            )*/
+            LoginComponent()
+
     }
 }
+
+
+/*@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    NutriappTheme {
+        Greeting("Android")
+    }
+}*/
+
