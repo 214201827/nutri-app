e0dc780ccd092f48dedda869a734779bcd690fd5
David Munguía Acosta <a214201827@unison.mx>
2024-09-16 13:36:15 -0700

    Agregadas las librerías de Firebase.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+
+    //id("com.android.application")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -67,4 +70,7 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+    implementation(platform("com.google.firebase:firebase-bom:33.3.0"))
+    implementation("com.google.firebase:firebase-analytics")
 }
