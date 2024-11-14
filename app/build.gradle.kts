16628b178ccd39014d8878a97de0b04bf1f36c70
David Munguía Acosta <a214201827@unison.mx>
2024-11-14 10:09:15 -0700

    Agregado "mostrar contraseña" y limitación a 16 caracteres.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,6 +64,7 @@ dependencies {
     implementation(libs.firebase.firestore.ktx)
     implementation(libs.firebase.storage.ktx)
     implementation(libs.material)
+    implementation(libs.androidx.espresso.core)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
