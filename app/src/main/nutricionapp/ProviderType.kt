9047568eb4ff63328f979d8f0b709b890afcf411
gafl236 <81320572+gafl236@users.noreply.github.com>
2024-11-28 08:45:26 -0700

    Delete app/src/main/nutricionapp directory

--- app/src/main/nutricionapp/ProviderType.kt
+++
@@ -1,5 +0,0 @@
-package com.example.nutricionapp
-
-enum class ProviderType {
-    BASIC
-}
--- app/src/main/nutricionapp/UserType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.nutricionapp
-
-enum class UserType {
-    ADMIN,
-    NUTRIOLOGO,
-    PACIENTE
-}
